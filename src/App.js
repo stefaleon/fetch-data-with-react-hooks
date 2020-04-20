df43181d7cb22a1dc9ea46b025f564c630fe3a04
stefaleon <stefaleon@gmail.com>
2020-04-20 17:32:05 +0300

    search-on-input-keystroke

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
   const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('hack');
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        'https://hn.algolia.com/api/v1/search?query=hack'
+        `https://hn.algolia.com/api/v1/search?query=${query}`
       );
       setData(result.data);
     };
     fetchData();
-  }, []);
+  }, [query]);
 
   return (
-    <ul>
-      {data.hits.map((item) => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <input
+        type='text'
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+      />
+      <ul>
+        {data.hits.map((item) => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
